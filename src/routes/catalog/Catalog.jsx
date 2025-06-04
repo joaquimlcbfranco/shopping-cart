b9f1f498ea8d92383535ad965423190faa40ee8c
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-04 14:51:38 +0100

    Initial commit

---
+++ src/routes/catalog/Catalog.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header.jsx";
+
+const Catalog = () => {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
+
+export default Catalog;
---
+++ src/routes/error-page/ErrorPage.jsx
@@ -0,0 +1,5 @@
+const ErrorPage = () => {
+	return <h1>Looks like you have gone too far!</h1>;
+};
+
+export default ErrorPage;
