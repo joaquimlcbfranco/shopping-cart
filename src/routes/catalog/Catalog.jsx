2df2bf562121d755e09782747d1a3f9012449525
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-04 16:09:33 +0100

    feat: Change catalogWrapper to wrapper, improves readability

--- src/routes/catalog/Catalog.jsx
+++ src/routes/catalog/Catalog.jsx
@@ -1,9 +1,17 @@
 import Header from "../../components/header/Header.jsx";
+import styles from "./Catalog.module.css"
+import Card from "../../components/card/Card.jsx"
 
 const Catalog = () => {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<Header />
+      <div className={styles.title}>
+        <span><h1>All items <p>[37]</p></h1></span>
+      </div>
+      <div>
+        <Card />
+      </div>
 		</div>
 	);
 };
