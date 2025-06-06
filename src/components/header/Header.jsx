dd3593c4416248a503702281920c31e73f71659a
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 15:18:22 +0100

    feat: Remove unused wrapper

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,24 +3,26 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div className={styles.headerWrapper}>
-			<header className={styles.navBar}>
-				<div className={styles.navBarLeft}>
-					<ul>
-						<li>
-							<Link className={styles.headerLinks} to="/">Home</Link>
-						</li>
-						<span></span>
-						<li>
-							<Link className={styles.headerLinks} to="/catalog">Catalog</Link>
-						</li>
-					</ul>
-				</div>
-				<div className={styles.navBarRight}>
-					<a className={styles.headerLinks}>Cart</a>
-				</div>
-			</header>
-		</div>
+		<header className={styles.navBar}>
+			<div className={styles.navBarLeft}>
+				<ul>
+					<li>
+						<Link className={styles.headerLinks} to="/">
+							Home
+						</Link>
+					</li>
+					<span></span>
+					<li>
+						<Link className={styles.headerLinks} to="/catalog">
+							Catalog
+						</Link>
+					</li>
+				</ul>
+			</div>
+			<div className={styles.navBarRight}>
+				<a className={styles.headerLinks}>Cart</a>
+			</div>
+		</header>
 	);
 };
 
