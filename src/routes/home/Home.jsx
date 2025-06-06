3f6d33f7716db594e79db3624f5beab1601ab7a9
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 10:23:53 +0100

    feat: Create new content wrapper, set position relative to position bottle image

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,20 +1,23 @@
 import Header from "../../components/header/Header.jsx";
 import styles from "./Home.module.css";
-import product from "../../assets/product.png"
+import product from "../../assets/product.png";
+import { Link } from "react-router-dom"
 
 const Home = () => {
 	return (
 		<div className={styles.wrapper}>
-			<Header />
-			<main className={styles.mainWrapper}>
-				<div className={styles.homeHero}>
-					<h1>Where convenience</h1>
-					<h1>Meets choice</h1>
-					<p>Your first stop for online shopping</p>
-					<a href="/">Catalog</a>
-				</div>
-        <img src={product} alt="example of website products"></img>
-			</main>
+			<div className={styles.relativeWrapper}>
+				<Header />
+				<main className={styles.mainWrapper}>
+					<div className={styles.homeHero}>
+						<h1>Where convenience</h1>
+						<h1>Meets choice</h1>
+						<p>Your first stop for online shopping</p>
+						<Link to="catalog">Catalog</Link>
+					</div>
+					<img src={product} alt="example of website products"></img>
+				</main>
+			</div>
 		</div>
 	);
 };
