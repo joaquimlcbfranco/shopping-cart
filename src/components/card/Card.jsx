a31599e8a3b880bd6312ade4a366be1a339eb5c0
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 13:20:58 +0100

    feat: Dinamically load price, add id to props

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import styles from "./Card.module.css";
 import { useState } from "react";
 
-const Card = ({ title, price, url, loading, error, handleQuantity }) => {
+const Card = ({ id, title, price, url, loading, error, handleQuantity }) => {
 	const [quantity, setQuantity] = useState(0);
 
 	const handleChange = (e) => {
@@ -25,33 +25,36 @@ const Card = ({ title, price, url, loading, error, handleQuantity }) => {
 					height: "250px",
 					backgroundRepeat: "no-repeat",
 					backgroundPosition: "center",
-					backgroundSize: "150px",
-          borderRadius: "5px 5px 0 0",
+					backgroundSize: "120px",
+					borderRadius: "5px 5px 0 0",
+          marginBottom: "auto",
 				}}
-			></div>
-			<div className={styles.price}>
-				<p>$42</p>
-			</div>
-			<div className={styles.title}>
-				<h2>{title}</h2>
-			</div>
-			<div className={styles.shopActions}>
-				<div className={styles.controls}>
-					<button onClick={decreaseQuantity}>-</button>
-					<input
-						type="text"
-						placeholder=" "
-						name="quantity"
-						min="0"
-						value={quantity}
-						onChange={(e) => handleChange(e)}
-						className={styles.input}
-						pattern="^[0-9]*$"
-					></input>
-					<button onClick={increaseQuantity}>+</button>
+			aria-label="image of product"></div>
+			<div className={styles.bottomCard}>
+				<div className={styles.price}>
+					<p>${price}</p>
 				</div>
-				<div>
-					<button className={styles.btn}>Add to cart</button>
+				<div className={styles.title}>
+					<h2>{title}</h2>
+				</div>
+				<div className={styles.shopActions}>
+					<div className={styles.controls}>
+						<button onClick={decreaseQuantity}>-</button>
+						<input
+							type="text"
+							placeholder=" "
+							name="quantity"
+							min="0"
+							value={quantity}
+							onChange={(e) => handleChange(e)}
+							className={styles.input}
+							pattern="^[0-9]*$"
+						></input>
+						<button onClick={increaseQuantity}>+</button>
+					</div>
+					<div>
+						<button className={styles.btn}>Add to cart</button>
+					</div>
 				</div>
 			</div>
 		</div>
