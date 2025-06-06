29a654f4ed0e8afe447a594b7dbed8314fd2544e
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 15:19:00 +0100

    feat: Remove unused wrapper

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,23 +1,21 @@
 import Header from "../../components/header/Header.jsx";
 import styles from "./Home.module.css";
 import product from "../../assets/product.png";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.relativeWrapper}>
-				<Header />
-				<main className={styles.mainWrapper}>
-					<div className={styles.homeHero}>
-						<h1>Where convenience</h1>
-						<h1>Meets choice</h1>
-						<p>Your first stop for online shopping</p>
-						<Link to="catalog">Catalog</Link>
-					</div>
-					<img src={product} alt="example of website products"></img>
-				</main>
-			</div>
+			<Header />
+			<main className={styles.mainWrapper}>
+				<div className={styles.homeHero}>
+					<h1>Where convenience</h1>
+					<h1>Meets choice</h1>
+					<p>Your first stop for online shopping</p>
+					<Link to="catalog">Catalog</Link>
+				</div>
+				<img src={product} alt="example of website products"></img>
+			</main>
 		</div>
 	);
 };
