b2823b76f9e2b02a19d8d23d8a678cfb92963c6e
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 10:28:09 +0100

    feat: Create new wrapper to set max-width

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,27 +3,24 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<header className={styles.navBar}>
-			<div className={styles.navBarLeft}>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-          <span></span>
-					<li><Link to="catalog">Catalog</Link></li>
-				</ul>
-			</div>
-			<div className={styles.navBarRight}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24px"
-					viewBox="0 -960 960 960"
-					width="24px"
-					fill="#000000"
-				>
-					<path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
-				</svg>
-        <li>Cart</li>
-			</div>
-		</header>
+		<div className={styles.headerWrapper}>
+			<header className={styles.navBar}>
+				<div className={styles.navBarLeft}>
+					<ul>
+						<li>
+							<Link className={styles.headerLinks} to="/">Home</Link>
+						</li>
+						<span></span>
+						<li>
+							<Link className={styles.headerLinks} to="catalog">Catalog</Link>
+						</li>
+					</ul>
+				</div>
+				<div className={styles.navBarRight}>
+					<a className={styles.headerLinks}>Cart</a>
+				</div>
+			</header>
+		</div>
 	);
 };
 
