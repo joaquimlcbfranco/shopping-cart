0da003203585b06031d5342e07c7bd067b1d90ca
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-06 13:24:27 +0100

    feat: Wrap App with CartProvider, move Home and Catalog to be children of App

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,16 +1,22 @@
+import CartProvider from "../context/CartContext.jsx";
 import App from "../App.jsx";
+import Home from "./home/Home.jsx";
 import Catalog from "./catalog/Catalog.jsx";
 import ErrorPage from "./error-page/ErrorPage.jsx";
 
 const routes = [
 	{
 		path: "/",
-		element: <App />,
+		element: (
+			<CartProvider>
+				<App />
+			</CartProvider>
+		),
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "catalog", element: <Catalog /> },
+		],
 		errorElement: <ErrorPage />,
-	},
-	{
-		path: "catalog",
-		element: <Catalog />,
 	},
 ];
 
