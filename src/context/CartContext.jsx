2dbe91273e9636560f81c9ab0093abbe343ea2e5
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-09 15:32:28 +0100

    feat: Create cart state variable, gets used by routes through context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-	const [cart, setCart] = useState(["test"]);
+	const [cart, setCart] = useState([]);
 
 	return (
 		<CartContext.Provider value={{cart, setCart}}>
