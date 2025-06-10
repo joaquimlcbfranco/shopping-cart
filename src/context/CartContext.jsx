0f3b467666febdf2f7603ad20f5936dc760270ab
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-06-10 09:07:29 +0100

    feat: Create new boolean state variable that is true if the cart is open, pass as context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,13 +1,17 @@
 import { useState, createContext } from "react";
 
 export const CartContext = createContext();
+export const CartOpenContext = createContext();
 
 const CartProvider = ({ children }) => {
 	const [cart, setCart] = useState([]);
+	const [cartOpen, setCartOpen] = useState(false);
 
 	return (
-		<CartContext.Provider value={{cart, setCart}}>
-			{children}
+		<CartContext.Provider value={{ cart, setCart }}>
+			<CartOpenContext.Provider value={{ cartOpen, setCartOpen }}>
+				{children}
+			</CartOpenContext.Provider>
 		</CartContext.Provider>
 	);
 };
